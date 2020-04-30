8a63d25c151acfb85bac65f97a7db565ef53dcbf
ditrihFrid07 <39777866+ditrihFrid07@users.noreply.github.com>
2020-04-30 20:39:43 +0300

    web-develpment
    web-develpment start project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//down
+$(document).ready(function(){
+    const anchors = document.querySelectorAll('a[href*="#"]');
+    for (let anchor of anchors) {
+        anchor.addEventListener("click", function(event){
+            event.preventDefault();
+            const blockID = anchor.getAttribute('href');
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+        });
+    }
+});
